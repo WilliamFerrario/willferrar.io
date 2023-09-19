a93aa24f74d80e13ef2a6fc86a65b0983ccf410d
WilliamFerrario <81998132+WilliamFerrario@users.noreply.github.com>
2023-09-19 11:46:58 -0400

    Second Commit *Responsive Updates*
    Added several responsive and  changes.
    
    function generateNoise(ctx) in script.js - takes a 2d canvas and generates noise as a canvas if a random value in the buffer is < .4 and updates every 120 ms, (updates < 90 ms update cause awful cursor update performance. 120 - 200 found to be most visually pleasing while still well optimized).
    
    function animate() in script.js - adds a 'springiness' effect to mouse movements. Using velocity, drag and strength from mouse X and Y paired with Cubic-Bezier
    (#custom-cursor in style.css) to create a 'try to catch up, overshoot, and spring back' effect to the actual cursor.
    
    *fixed h2 elements not being able to grow on hover

--- script.js
+++ script.js
@@ -1,10 +1,75 @@
 const cursor = document.getElementById('custom-cursor');
 const abouts = document.querySelectorAll('a');
+const canvas = document.getElementById('noiseCanvas');
+const ctx = canvas.getContext('2d');
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function generateNoise(ctx) {
+    const w = ctx.canvas.width;
+    const h = ctx.canvas.height;
+    const idata = ctx.createImageData(w, h);
+    const buffer32 = new Uint32Array(idata.data.buffer);
+    const len = buffer32.length;
+
+    for (let i = 0; i < len; i++) {
+        if (Math.random() < 0.4) {
+            buffer32[i] = 0xff212121;  // Black
+        } else {
+            buffer32[i] = 0xff242424;  // Dark gray
+        }
+    }
+    ctx.putImageData(idata, 0, 0);
+}
+
+function loop() {
+    generateNoise(ctx);
+}
+
+setInterval(loop, 120);  // Update every 150 milliseconds (half a second)
+
+// Event to handle window resize
+window.addEventListener('resize', resizeCanvas);
+
+// Initial setup
+resizeCanvas();
+
+// ---------mouse positioning and custom cursor movements-----------
+let mouseX = 0, mouseY = 0;
+let cursorX = 0, cursorY = 0;
+let velocityX = 0, velocityY = 0;
+const drag = 0.2;
+const strength = 0.05;
+
 
 document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
+    mouseX = e.pageX;
+    mouseY = e.pageY;
 });
+
+function animate() {
+    let accelX = (mouseX - cursorX) * strength;
+    let accelY = (mouseY - cursorY) * strength;
+
+    velocityX += accelX;
+    velocityY += accelY;
+    velocityX *= (1 - drag);
+    velocityY *= (1 - drag);
+
+    cursorX += velocityX;
+    cursorY += velocityY;
+
+    cursor.style.transform = `translate(${cursorX}px, ${cursorY}px)`;
+
+    requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
+
+//--------------------------------------------------------------------
 
 document.addEventListener('mouseenter', () => {
     cursor.style.display = 'block'; 
